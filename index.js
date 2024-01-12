f20bd792bb0ce5f8d1a50216a908d80b8a48128c
smntsng <144932270+smntsng@users.noreply.github.com>
2024-01-12 11:43:27 +0000

    updated inqurier with prompts to ask managers, eningeers and interns their details using the when object to determin the user

--- index.js
+++ index.js
@@ -12,4 +12,79 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const team = inquirer.prompt([
+    
+    {
+        prefix: 'Please build your team',
+        type: 'input',
+        message: 'What is the Team Manager\'s name?',
+        name: 'managerName',
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Team Manager\'s id?',
+        name: 'managerId',
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Team Manager\'s email?',
+        name: 'managerEmail',
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Team Manager\'s office number?',
+        name: 'managerOfficeNumber',
+    },
+
+    {
+        type: 'list',
+        message: 'Which type of team member would you like to add?',
+        name: 'teamMember',
+        choices: ['Engineer',
+          'Intern',
+          'i don\'t want to add anymore team members.'],
+    },
+
+    
+    {
+        type: 'input',
+        message: 'What is the Engineer\'s name?',
+        name: 'engineerName',
+        when:function (answers) {
+            return answers.teamMember === 'Engineer';
+    },
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Engineer\'s id?',
+        name: 'engineerId',
+        when:function (answers) {
+            return answers.teamMember === 'Engineer';
+    },
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Engineer\'s email?',
+        name: 'engineerEmail',
+        when:function (answers) {
+            return answers.teamMember === 'Engineer';
+    },
+    },
+
+    {
+        type: 'input',
+        message: 'What is the Engineer\'s Github username?',
+        name: 'engineerGithub',
+        when:function (answers) {
+            return answers.teamMember === 'Engineer';
+    },
+    },
+    
+
+]);
 
